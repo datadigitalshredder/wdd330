810a9e926af749cb65482e62de56ed527ef49644
datadigitalshredder <hov21003@byui.edu>
2022-04-22 13:02:14 +0200

    Week 1 Work

--- js/main.js
+++ js/main.js
@@ -1,14 +1,69 @@
-// Week 1 - Portfolio bulid
+// ADDING THE YEAR
+// Get the element with the id=year and assign it a variable
+const year = document.getElementById("year");
+// Assign to that element the full year
+year.innerHTML = new Date().getFullYear();
+//ADDING THE LAST UPDATED DATE
+// Get the element with the id=lastupdate and assign it a variable
+const lastupdate = document.querySelector('#lastupdate');
+// To that variable assign the template literal with text, Last updated, plus uses the property lastModified from the page.
+lastupdate.innerHTML = `<strong>Last updated</strong>: ${document.lastModified}`;
+
+// Week 1 - Portfolio build
 // Manipulating the DOM
 //Create table of contents for the portfolio
 const links = [
     {
-      label: "Week1 notes:",
+      label: "Week1 notes and exercises",
       url: "week1/index.html"
     },
     {
-    label: "Week2 notes:",
+    label: "Week2 notes and exercises",
     url: "week2/index.html"
+    },
+    {
+      label: "Week3 notes and exercises",
+      url: "week3/index.html"
+    },
+    {
+      label: "Week4 notes and exercises",
+      url: "week4/index.html"
+    },
+    {
+      label: "Week5 notes and exercises",
+      url: "week5/index.html"
+    },
+    {
+      label: "Week6 notes and exercises",
+      url: "week6/index.html"
+    },
+    {
+      label: "Week7 notes and exercises",
+      url: "week7/index.html"
+    },
+    {
+      label: "Week8 notes and exercises",
+      url: "week8/index.html"
+    },
+    {
+      label: "Week9 notes and exercises",
+      url: "week9/index.html"
+    },
+    {
+      label: "Week10 notes and exercises",
+      url: "week10/index.html"
+    },
+    {
+      label: "Week11 notes and exercises",
+      url: "week11/index.html"
+    },
+    {
+      label: "Week12 notes and exercises",
+      url: "week12/index.html"
+    },
+    {
+      label: "Week13 notes and exercises",
+      url: "week13/index.html"
     }
   ]
 
@@ -17,20 +72,19 @@ const tableListItems = links;
 tableListItems.forEach(tableItems);
 
 function tableItems(item) {
-    // Create elements and assign them variable
-    let portfolioItem = document.createElement('span'); 
-    let listItem = document.createElement('li'); 
-    let url = document.createElement('a');
+    // Get the document to create the anhor tag and assign it a variable, weekLink
+    let weekLink = document.createElement('a');
 
-    // Assign contents of the array to the respective varibles with the elements
-    listItem.textContent = `${item.label}`;
-    url.textContent = `${item.url}`;
+    // Assign contents of the array to the anchor variable, weekLink
+    weekLink.innerHTML = item.label;
+    weekLink.href = item.url; 
+    
+    // Get the document to create the list items, li, and assign that to the variable listItem
+    let listItem = document.createElement('li');
 
-    // Append the populated variables to the span in HTML
-    portfolioItem.appendChild(listItem);
-    portfolioItem.appendChild(url)
+    // Append the populated variable (weekLink - it had the URL) to the list items in HTML
+    listItem.appendChild(weekLink);
 
-    // Populate the ordered list with contents from the array
-    document.querySelector('ol.portfolio-table').appendChild(portfolioItem);
-
+    // Get the document to populate the ordered list with contents from the array, which are now the variable listItem
+    document.querySelector('ol.portfolio-table').appendChild(listItem);
 }
