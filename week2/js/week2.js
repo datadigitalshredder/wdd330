68dced474a123e4200029c0fbbe89f63a32c567b
datadigitalshredder <hov21003@byui.edu>
2022-04-25 16:34:24 +0200

    Week 2 work

---
+++ week2/js/week2.js
@@ -0,0 +1,16 @@
+// TYPEOF
+console.log(typeof false);
+
+console.log(typeof 13);
+
+console.log(typeof { ninja: 'turtle' });
+
+// LET VARIABLE
+let points = 25;
+console.log(points);
+
+points = 40;
+console.log(points);
+
+points = 100;
+console.log(points);
