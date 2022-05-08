7a5b6c99124de87eb4bf15eeb72e734ffca7229d
datadigitalshredder <hov21003@byui.edu>
2022-05-08 05:42:57 +0200

    Week3 work4

--- week2team/js/week2.js
+++ week2team/js/week2.js
@@ -5,27 +5,31 @@ const read = () => {
 }
 // Core requirement 2
 const readNumber = () => {
-    const userInput = document.getElementById('button-2').value; // Get the number value the user entered
+    const userInput = document.getElementById('number-1').value; // Get the number value the user entered
     const number = parseInt(userInput); // Note: the value will be string, so convert it to a number
-    const userArray = [...Array(number+1).keys()].slice(1); // Use the spread method to create an array starting at one. .slice(1) will start adding values at 1 and increment by 1.
-    console.log(userArray);
-    const total = userArray.reduce( (acc,val) => acc + val ); // Use the accumulation method to sum the array
-    document.querySelector('#number-1').innerHTML = total; // Display the upto to and including the user number
+    // Using the reduce method
+    // const userArray = [...Array(number+1).keys()].slice(1); // Use the spread method to create an array starting at one. .slice(1) will start adding values at 1 and increment by 1.
+    // const total = userArray.reduce( (acc,val) => acc + val ); // Use the accumulation method to sum the array
+    // Using a for loop
+    let total = 0;
+    for (let i = 0; i <= number; i++)
+    total += i;
+    document.querySelector('#acc-total').innerHTML = total; // Display the upto to and including the user number
 }
 // Core requirement 3
-function numbers (a, b) {
-    return a + b;
-}
 const addNumbers = () => {
-    const userInputOne = document.getElementById('number-1').value; // Get the number value the user entered
-    const userInputTwo = document.getElementById('number-2').value;
-    // console.log(typeof userInputOne);
+    const userInputOne = document.getElementById('number-2').value; // Get the number values the user entered
+    const userInputTwo = document.getElementById('number-3').value;
 
-    // const numberOne = parseFloat(userInputOne); // Note: the value will be string, so convert it to a number
-    // const numberTwo = parseFloat(userInputTwo);
-    // console.log(parseFloat(numberOne));
-
-     const sum = numbers(parseFloat(userInputOne.value), parseFloat(userInputTwo.value));
-    // const sum = parseFloat(numberOne) + parseFloat(numberTwo);
+    const numberOne = parseFloat(userInputOne); // Note: the values will be string, so convert them to a numbers
+    const numberTwo = parseFloat(userInputTwo);
+    const sum = numberOne + numberTwo; // add the two numbers
     document.querySelector('#add-numbers').innerHTML = sum; // Display the upto to and including the user number
+}
+// Stretch requirement - the Calculator
+const number = () => {
+    const numberEight = parseInt(document.getElementById('eight').value);
+    const plus = document.getElementById('plus').value;
+    document.querySelector('#total').innerHTML = numberEight;
+    document.querySelector('#total').innerHTML = plus;
 }
