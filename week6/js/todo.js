88bde743220b3a7a2c5dbdd4a331387cb98f61dc
datadigitalshredder <hov21003@byui.edu>
2022-05-18 16:30:35 +0200

    Week 5 notes2

--- week6/js/todo.js
+++ week6/js/todo.js
@@ -1,3 +1,7 @@
+/*
+CODE HERE IS PRIVATE - reads and write localStorage
+*/
+
 import { querySelection, writeToLocalStorage, readFromLocalStorage, bindTouch } from "./controls.js";
 
 let liveToDos = null;
@@ -6,6 +10,7 @@ let liveToDos = null;
 function renderList (list, element, toDos, hidden) {
     console.log(list); // Start by displaying the list
     element.innerHTML = '';
+    // elementDate.innerHTML = '';
 
     list.forEach(todoItem => {
         const item = document.createElement('li');
@@ -15,9 +20,13 @@ function renderList (list, element, toDos, hidden) {
         let button = null;
 
         if(hidden && todoItem.completed) {
-            item.innerHTML = `<label><input type="checkbox" checked><strike> ${todoItem.content}</strike></label>`
+            item.innerHTML = `<label><input type="checkbox" checked><strike>${todoItem.content}</strike></label>`;
+            // formattedDate.innerHTML = `<label><input type="date" checked><strike>${todoItem.content}</strike></label>`;
+            
         } else {
-            item.innerHTML = `<label><input type="checkbox"> ${todoItem.content}</label><button>❌</buuton>`
+            item.innerHTML = `<label><input type="checkbox"> ${todoItem.content}</label><button>❌</buton>`;
+            // elementDate.innerHTML = `${formattedDate}`;
+
         }
 
         // attach event listener to the checkbox
@@ -38,6 +47,7 @@ function renderList (list, element, toDos, hidden) {
         }
 
         element.appendChild(item);
+        // elementDate.appendChild(formattedDate);
     })
 }
  
@@ -50,7 +60,7 @@ function getToDos(key) {
 }
 
 function addToDo(value, key) {
-    // add the todo with Date using UTC millesecond string
+    // add the todo with Date using UTC millesecond string as the id
     const newToDoItem = {
         id: new Date(),
         content: value,
@@ -60,6 +70,64 @@ function addToDo(value, key) {
     writeToLocalStorage(key, liveToDos);
 }
 
-function deleteTodo(key) { // line 71
+function deleteTodo(key) { 
+    let newList = liveToDos.filter(item => item.id != key);
+    liveToDos = newList;
+    writeToLocalStorage(key, liveToDos);
+}
 
+function filterToDos(key, completed = true) {
+    let toDos = getToDos(key);
+
+    // return a list of completed or uncompleted todos based on the parameter
+    return toDos.filter(item => item.completed === hidden);
+}
+
+// CODE HERE ONWARDS IS PUBLIC
+export default class ToDoItems {
+    constructor(listElement, key) {
+        this.listElement = listElement; // Store the listelement inside the class
+        this.key = key;
+        bindTouch("#addToDo", this.newToDo.bind(this));
+        this.listToDos();
+    }
+
+    newToDo() {
+        const newTask = document.getElementById("todoInput");
+        addToDo(newTask.value, this.key);
+        newTask.value = "";
+        this.listToDos();
+    }
+
+    findToDo(id) {
+        let toDo = liveToDos.find( element => {
+            return element.id === id;
+        });
+        return toDo;
+    }
+
+    completeToDoItem(id) {
+        console.log(id + 'checked');
+        let toDo = this.findToDo(id);
+
+        if(toDo){
+            toDo.completed = !toDo.completed;
+            writeToLocalStorage(this.key, liveToDos);
+            renderList(liveToDos, this.listElement, this, true);
+        }
+    }
+
+    removeToDoItem(id) {
+        console.log(id + 'removed');
+        let toDo = this.findToDo(id);
+
+        if(toDo){
+            deleteTodo(id);
+            renderList(liveToDos, this.listElement, this, true);
+        }
+    }
+
+    listToDos(hidden = true) {
+        renderList(getToDos(this.key), this.listElement, this, hidden);
+    }
 }
