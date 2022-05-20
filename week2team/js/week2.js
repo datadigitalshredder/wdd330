5e88c6d9a731e5d243e62821577b1cc77d455f8f
datadigitalshredder <hov21003@byui.edu>
2022-05-20 12:29:45 +0200

    Complete calculator

--- week2team/js/week2.js
+++ week2team/js/week2.js
@@ -26,10 +26,38 @@ const addNumbers = () => {
     const sum = numberOne + numberTwo; // add the two numbers
     document.querySelector('#add-numbers').innerHTML = sum; // Display the upto to and including the user number
 }
-// Stretch requirement - the Calculator
+// Stretch requirement - simple Calculator
 const number = () => {
     const numberEight = parseInt(document.getElementById('eight').value);
     const plus = document.getElementById('plus').value;
     document.querySelector('#total').innerHTML = numberEight;
     document.querySelector('#total').innerHTML = plus;
 }
+
+// Complete Calculator
+let display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case '←':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
