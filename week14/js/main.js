1d746a8eb2a24f3ca1b2ebcbd24b65cba24f1603
datadigitalshredder <hov21003@byui.edu>
2022-07-21 05:58:55 +0200

    Week 14 final submission 2

---
+++ week14/js/main.js
@@ -0,0 +1,16 @@
+// Basic app functionality: dates and menu
+
+// The header menu button
+function toggleMenu (){
+    document.getElementById('primaryNavigation').classList.toggle('open');
+    document.getElementById('hamburgerButton').classList.toggle('open');
+
+}
+const btn = document.getElementById('hamburgerButton');
+btn.onclick = toggleMenu;
+
+// Date year in the footer
+const year = document.querySelector("#year");
+year.innerHTML = new Date().getFullYear();
+const lastmod = document.querySelector('#lastmod');
+// lastmod.innerHTML = `<strong>Last updated</strong>: ${document.lastModified}`;
